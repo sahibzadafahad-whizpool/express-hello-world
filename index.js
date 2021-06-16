726f401b081b3e62d20a6fe5acddaeaec3157f99
sahibzadafahad-whizpool <sahibzada.fahad@whizpool.com>
2021-06-16 14:53:17 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello i am testing github actions and webhooks and test')
+  res.send('Hello i am testing github actions and webhooks and test actions')
 })
 
 app.listen(3000, function () {
