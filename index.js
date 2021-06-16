b9411fa08bb112a80f617a76c0a6da6a46f646c7
sahibzadafahad-whizpool <sahibzada.fahad@whizpool.com>
2021-06-16 16:05:37 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello this code will auto deploy on server through GitHub Actions')
+  res.send('Hello this code will auto deploy on server through GitHub Actions1')
 })
 
 app.listen(3000, function () {
